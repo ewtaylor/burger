43a7550a21a3f0b4db3a782a9f7ff99ca8b6e05d
erin taylor <ewtaylor29@gmail.com>
2020-01-27 21:43:23 -0500

    connection

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Select burgers_db
+USE burgers_db;
+
+-- insert 3 burgers
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bison', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Wagyu', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', false);
