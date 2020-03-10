b832ed4c6a95714debf122523549c34e87ec22bb
erin taylor <ewtaylor29@gmail.com>
2020-03-09 23:04:47 -0400

    deploy and pic change

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-
 var router = require('./controllers/burgers_controllers.js');
 app.use(router);
 
